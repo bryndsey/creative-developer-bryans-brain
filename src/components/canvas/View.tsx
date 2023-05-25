d997e76e8a6c1def9fbbeaf39f0aab30f507f0c4
Bryan <blinz117@gmail.com>
2023-05-25 08:52:57 -0500

    Start switching over to my content

--- src/components/canvas/View.tsx
+++ src/components/canvas/View.tsx
@@ -10,7 +10,7 @@ export const Common = ({ color }) => (
     <ambientLight intensity={0.5} />
     <pointLight position={[20, 30, 10]} intensity={1} />
     <pointLight position={[-10, -10, -10]} color='blue' />
-    <PerspectiveCamera makeDefault fov={40} position={[0, 0, 6]} />
+    <PerspectiveCamera makeDefault fov={40} position={[0, 0, 3]} />
   </Suspense>
 )
 
