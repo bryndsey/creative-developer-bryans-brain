ca00480f0e67cad97b85096602d4b601760f6886
Bryan <blinz117@gmail.com>
2023-05-29 09:31:05 -0500

    Improve getting viewport for accuracy
    Also added some performance monitoring so I can keep an eye on how performant things are

--- src/songspark/SongSparkHero.tsx
+++ src/songspark/SongSparkHero.tsx
@@ -1,13 +1,54 @@
 'use client'
 
-import { Common, View } from '@/components/canvas/View'
+import { View } from '@/components/canvas/View'
+import { PerspectiveCamera } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+import useMeasure from 'react-use-measure'
+import { Group, PerspectiveCamera as ThreePerspectiveCamera, Vector3 } from 'three'
 import { AcousticGuitar } from './AcousticGuitar'
 
-export function SongSparkHero() {
+const viewportTarget = new Vector3()
+const guitarPosition = new Vector3()
+function HeroContent({ bounds }) {
+  const guitarRef = useRef<Group>(null!)
+  const cameraRef = useRef<ThreePerspectiveCamera>(null!)
+  useFrame((state) => {
+    const myViewport = state.viewport.getCurrentViewport(cameraRef.current, viewportTarget, bounds)
+    // console.log(state.size)
+    // console.log(cameraRef.current.view.width, cameraRef.current.view.height)
+    // console.log(cameraRef.current)
+
+    // console.log('Width', myViewport.width, 'Height', myViewport.height)
+    guitarRef.current.position.x = myViewport.width / 4
+
+    //   console.log(state.size.width)
+    // console.log(state.viewport.factor)
+
+    // guitarPosition.set(0.5, -0.25, 0.92)
+    // const newPosition = guitarPosition.unproject(cameraRef.current)
+    // guitarRef.current.position.set(newPosition.x, newPosition.y, newPosition.z)
+    // console.log(newPosition)
+    guitarRef.current.rotation.set(Math.PI / 2, -0.25, 0.75)
+  })
+
   return (
-    <View className='h-full w-full'>
-      <AcousticGuitar />
-      <Common />
+    <>
+      <group ref={guitarRef}>
+        <AcousticGuitar />
+      </group>
+      <ambientLight />
+      <PerspectiveCamera ref={cameraRef} makeDefault fov={40} far={10} position={[0, 0, 3]} />
+    </>
+  )
+}
+
+export function SongSparkHero() {
+  const [ref, bounds] = useMeasure()
+
+  return (
+    <View className='h-full min-h-[400px] w-full' ref={ref}>
+      <HeroContent bounds={bounds} />
     </View>
   )
 }
