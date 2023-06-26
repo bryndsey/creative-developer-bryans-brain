a79f79599671c3fe07356394c0779c17d8239f69
Bryan <blinz117@gmail.com>
2023-06-26 16:09:02 -0500

    Clean up some unneeded dependencies

--- src/components/dom/Layout.tsx
+++ src/components/dom/Layout.tsx
@@ -1,45 +1,38 @@
 'use client'
 
-import React, { useRef } from 'react'
 import dynamic from 'next/dynamic'
-import { MotionConfig } from 'framer-motion'
-import { ReactLenis } from '@studio-freight/react-lenis'
+import React, { useRef } from 'react'
 const Scene = dynamic(() => import('@/components/canvas/Scene'), { ssr: false })
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const ref = useRef()
 
   return (
-    <MotionConfig transition={{ duration: 0.5, ease: 'circOut' }}>
-      <ReactLenis root>
-        <div
-          ref={ref}
-          style={{
-            position: 'relative',
-            width: ' 100%',
-            height: '100%',
-            touchAction: 'auto',
-          }}
-        >
-          {children}
-          <Scene
-            style={{
-              position: 'fixed',
-              top: 0,
-              left: 0,
-              width: '100vw',
-              height: '100vh',
-              pointerEvents: 'none',
-              // zIndex: -1,
-            }}
-            eventSource={ref}
-            eventPrefix='client'
-            // orthographic
-            camera={{ fov: 40 }}
-          />
-        </div>
-      </ReactLenis>
-    </MotionConfig>
+    <div
+      ref={ref}
+      style={{
+        position: 'relative',
+        width: ' 100%',
+        height: '100%',
+        touchAction: 'auto',
+      }}
+    >
+      {children}
+      <Scene
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          pointerEvents: 'none',
+          // zIndex: -1,
+        }}
+        eventSource={ref}
+        eventPrefix='client'
+        camera={{ fov: 40 }}
+      />
+    </div>
   )
 }
 
