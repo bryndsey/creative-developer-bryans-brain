80cfe132b173131e48277d32a0a88ee3e05aab2b
Bryan <blinz117@gmail.com>
2023-06-26 08:13:51 -0500

    Extract component

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,10 @@ import { useAtom } from 'jotai'
 import { Suspense, useEffect, useRef, useState } from 'react'
 import { Group, MathUtils } from 'three'
 import { useAutoRotateValue, useAutoRotateAtom } from './useAutoRotateValue'
+import { MetaContent } from './MetaContent'
 
-const primaryTextColor = 'dimgrey'
-const secondaryTextColor = 'darkgrey'
+export const primaryTextColor = 'dimgrey'
+export const secondaryTextColor = 'darkgrey'
 
 function normalizeAngle(angle: number) {
   return MathUtils.euclideanModulo(angle, Math.PI * 2)
@@ -292,29 +293,6 @@ function ThreeContent() {
         </Center>
       </animated.group>
     </>
-  )
-}
-
-function MetaContent() {
-  return (
-    <div className='rounded-xl bg-white/60 p-8 [box-shadow:0_0_25px_25px_rgba(255,255,255,0.6)]'>
-      <p className='text-4xl'>Made by</p>
-      <div className='text-9xl font-extrabold leading-none' style={{ color: primaryTextColor }}>
-        <p>
-          <span className='line-through opacity-70' style={{ color: secondaryTextColor }}>
-            BRAIN
-          </span>
-          <br />
-          <span className='line-through opacity-70' style={{ color: secondaryTextColor }}>
-            BRIAN
-          </span>
-          <br />
-          <span className='leading-none'>BRYAN</span>
-        </p>
-        {/* This value is chosen for the ratio of size between first and last name, assuming monospace */}
-        <p className='text-[0.71em] leading-none'>LINDSEY</p>
-      </div>
-    </div>
   )
 }
 
