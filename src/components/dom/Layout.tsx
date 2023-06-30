27a22ce74b7e62a9182d9cde3653a2efd4778e98
Bryan <blinz117@gmail.com>
2023-06-30 14:36:16 -0500

    Better detection for mouse vs touch

--- src/components/dom/Layout.tsx
+++ src/components/dom/Layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import dynamic from 'next/dynamic'
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import 'react-creative-cursor/dist/styles.css'
 import { Cursor } from 'react-creative-cursor'
 const Scene = dynamic(() => import('@/components/canvas/Scene'), { ssr: false })
@@ -9,12 +9,27 @@ const Scene = dynamic(() => import('@/components/canvas/Scene'), { ssr: false })
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const ref = useRef()
 
+  const [hasMouseMoved, setMouseMoved] = useState(false)
+
   return (
     <>
       {/* Set cursor size to a non-zero but small number to effectively hide it. 
       There's an issue with setting it to zero where it messes up the alignment, which is why it isn't zero */}
-      <Cursor animationDuration={0.5} exclusionBackgroundColor='yellow' isGelly cursorSize={0.1} />
-      <div data-cursor-exclusion className='cursor-none bg-white' data-cursor-size={125}>
+      <Cursor
+        animationDuration={0.5}
+        exclusionBackgroundColor='yellow'
+        isGelly
+        cursorSize={hasMouseMoved ? 125 : 0.1}
+      />
+      <div
+        data-cursor-exclusion
+        className='cursor-none bg-white'
+        onPointerEnter={(e) => {
+          if (!hasMouseMoved && e.pointerType === 'mouse') {
+            setMouseMoved(true)
+          }
+        }}
+      >
         <div
           ref={ref}
           style={{
